20a5ad1aac47f0921c3e58dbfe3c03bf97498372
Afrie Irham <afrieirham.work@gmail.com>
2023-07-30 00:47:30 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>NEXT_PUBLIC_ENV_TEST: {env}</main>
+      <main className={styles.main}>
+        <p>NEXT_PUBLIC_ENV_TEST:</p>
+        <p>{env}</p>
+      </main>
     </>
   );
 }
